79fa914d7a9e950b67246231dbd98729e51b267d
Alex-R-Projects <mralexreyes99@gmail.com>
2024-04-22 14:40:44 -0700

    + Added main.py + Added gmap_api_key.py (Does not have key)

---
+++ gmap_api_key.py
@@ -0,0 +1,5 @@
+import googlemaps
+
+gmap = googlemaps.Client(key= 'Add key here')
+# Use your own key test the program, but be careful not to pull/push it into github
+
---
+++ main.py
@@ -0,0 +1,23 @@
+from gmap_api_key import *
+import matplotlib as plt # For Visualizations
+
+# For this project we are going to use the google maps API for be able to 1) Visualize, and 2) use as a graph that way we can implement the many shortest path algorithms 
+
+class Graph(object): # This will 
+    pass
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
