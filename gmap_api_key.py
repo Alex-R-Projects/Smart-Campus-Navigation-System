c29dc027ccc6f0027590f95fe0c3d0834600a699
Alex-R-Projects <mralexreyes99@gmail.com>
2024-04-24 13:06:14 -0700

    + Created a map/graph of CSUF campus using the osmnx library

--- gmap_api_key.py
+++ gmap_api_key.py
@@ -1,5 +1,8 @@
 import googlemaps
+import google_streetview.api
 
-gmap = googlemaps.Client(key= 'Add key here')
+gmap = googlemaps.Client(key= 'Your api key')
 # Use your own key test the program, but be careful not to pull/push it into github
 
+
+# CSUF Campus Cordinates: 33.8823° N, 117.8851° W
--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from gmap_api_key import *
 import matplotlib as plt # For Visualizations
-
+import osmnx as ox
 # For this project we are going to use the google maps API for be able to 1) Visualize, and 2) use as a graph that way we can implement the many shortest path algorithms 
 
-class Graph(object): # This will 
-    pass
+# Coordinates for CSUF
+latitude = 33.8823
+longitude = -117.8851
 
 
 
 
+# Retrieve the graph of the university campus
+# dist = distance in meters, we can change the parameter to adjust how detailed we want the graph to be 
+csuf_campus_map = ox.graph_from_point((latitude, longitude), dist=1000, network_type='all')
+# Network_type = pathways, like bike ways, streets, etc
 
+# Plotting the graph to visualize it
+ox.plot_graph(csuf_campus_map, node_size=0)
 
-
-
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
